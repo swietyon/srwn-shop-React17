da3dbed759406ccbf8bb0893e0b9c3ee3e6c036e
swietyon <32486978+swietyon@users.noreply.github.com>
2022-03-18 12:52:49 +0100

    added sing in component, and firebase.utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,12 @@ import {
     signInWithPopup, 
     GoogleAuthProvider 
 } from "firebase/auth";
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
 
 
 const firebaseConfig = {
@@ -31,3 +37,39 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'user', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }
+        catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+
+    //IF USER DATA EXISTS
+
+    //if UserData does not exists
+
+    //RETURN USERDOCREF
+}
+
