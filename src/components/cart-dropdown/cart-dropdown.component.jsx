fb7e4fd6d0e9f205e41a69f191b80d367d01a6e1
swietyon <32486978+swietyon@users.noreply.github.com>
2022-03-31 00:33:23 +0200

    changed cart-dropdown styles to jsx

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from 'react-router';
 
 import { useContext } from 'react';
 
-import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -17,12 +17,18 @@ const CartDropdown = () => {
   }
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
-        {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {
+          cartItems.length ? (cartItems.map(item => <CartItem key={item.id} cartItem={item} />)) 
+          : 
+          (
+            <EmptyMessage> Your cart is empty</EmptyMessage>
+          )
+        }
+      </CartItems>
         <Button buttonType='inverted' onClick={goToCheckoutHandler}>Go to checkout</Button>      
-    </div>
+    </CartDropdownContainer>
   );
 }
 
